bc63284fce7d8f5f5b79a84cb6e26e1804abb51c
Jin Yoon <jinyoon0124@gmail.com>
2017-05-10 16:13:51 -0700

    change create action to save the Article model data in the database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,14 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		render plain: params[:article].inspect
+		#below was the placeholder code for create method
+		#render plain: params[:article].inspect
+		#this is the actual code that will create article upon submission
+		@article = Article.new(params[:article])
+
+		@article.save
+		redirect_to @article
 	end
+
+	
 end
