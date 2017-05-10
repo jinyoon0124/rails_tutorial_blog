cb7e77da9c05608aed6200dc86dfef8ac5c9d232
Jin Yoon <jinyoon0124@gmail.com>
2017-05-10 16:15:16 -0700

    add article_params private method to 'whitelist' parameters... otherwise, attribute cannot be passed due to Strong Parameter security features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,14 @@ class ArticlesController < ApplicationController
 		#below was the placeholder code for create method
 		#render plain: params[:article].inspect
 		#this is the actual code that will create article upon submission
-		@article = Article.new(params[:article])
+		@article = Article.new(article_params)
 
 		@article.save
 		redirect_to @article
 	end
 
-	
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 end
